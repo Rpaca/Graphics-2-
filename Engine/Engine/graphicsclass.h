71ba0d7beaf5d4b94930324a7e8b0ef4ac495172
Rpaca <paysage11@naver.com>
2019-11-07 19:27:41 +0900

    PointLight
    커밋 오류 수정

---
+++ Engine/Engine/graphicsclass.h
@@ -0,0 +1,66 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: graphicsclass.h
+////////////////////////////////////////////////////////////////////////////////
+#ifndef _GRAPHICSCLASS_H_
+#define _GRAPHICSCLASS_H_
+
+
+///////////////////////
+// MY CLASS INCLUDES //
+///////////////////////
+#include "d3dclass.h"
+#include "cameraclass.h"
+#include "textclass.h"
+#include "modelclass.h"
+#include "lightshaderclass.h"
+#include "lightclass.h"
+#include "pointlightshaderclass.h"
+#include "pointlightclass.h"
+#include "textureshaderclass.h"
+#include "bitmapclass.h"
+#include "inputclass.h"
+#include "skyboxclass.h"
+
+/////////////
+// GLOBALS //
+/////////////
+const bool FULL_SCREEN = false;
+const bool VSYNC_ENABLED = true;
+const float SCREEN_DEPTH = 1000.0f;
+const float SCREEN_NEAR = 0.1f;
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Class name: GraphicsClass
+////////////////////////////////////////////////////////////////////////////////
+class GraphicsClass
+{
+public:
+	GraphicsClass();
+	GraphicsClass(const GraphicsClass&);
+	~GraphicsClass();
+
+	bool Initialize(int, int, HWND, InputClass*);
+	void Shutdown();
+	bool Frame(int, int, float);
+	bool Render(float);
+
+private:
+	D3DClass* m_D3D;
+	CameraClass* m_Camera;
+	vector<ModelClass*>		 m_Models;
+	LightShaderClass* m_LightShader;
+	LightClass* m_Light;
+	PointLightShaderClass* m_PointLightShader;
+	PointLightClass* m_PointLight;
+	InputClass* m_Input;
+	TextureShaderClass* m_TextureShader;
+	BitmapClass* m_Bitmap;
+	TextClass* m_Text;
+	SkyboxClass* m_Skybox;
+	vector<D3DXMATRIX>	 m_objMatrices;
+	int					 m_numOfPolygons;
+	float				 playerPosZ;
+};
+#endif
---
+++ Engine/Engine/inputclass.h
@@ -0,0 +1,62 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: inputclass.h
+////////////////////////////////////////////////////////////////////////////////
+#ifndef _INPUTCLASS_H_
+#define _INPUTCLASS_H_
+
+
+
+enum class KeyCode {
+	W, A, S, D,
+	ESCAPE, LSHFIT,
+	F1, F2,
+	LEFTARROW, RIGHTARROW
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Class name: InputClass
+////////////////////////////////////////////////////////////////////////////////
+class InputClass
+{
+public:
+	InputClass();
+	InputClass(const InputClass&);
+	~InputClass();
+
+	bool Initialize(HINSTANCE, HWND, int, int);
+	void Shutdown();
+	bool Frame();
+
+	bool GetKey(KeyCode key);
+	bool GetKeyDown(KeyCode key);
+	bool GetKeyUp(KeyCode key);
+
+	bool IsEscapePressed();
+	void GetMouseLocation(int&, int&);
+	void GetMouseDeltaPosition(int&, int&);
+	bool GetLBMouseDown();
+
+private:
+	bool ReadKeyboard();
+	bool ReadMouse();
+	void ProcessInput();
+
+private:
+	IDirectInput8* m_directInput;
+	IDirectInputDevice8* m_keyboard;
+	IDirectInputDevice8* m_mouse;
+
+	map<KeyCode, unsigned char>			m_keyState;
+
+	unsigned char	m_keyboardState[256];
+	unsigned char	m_pressedKeys[256];
+	DIMOUSESTATE m_mouseState;
+
+	int m_screenWidth = 0;
+	int m_screenHeight = 0;
+	int m_mouseX = 0;
+	int m_mouseY = 0;
+};
+
+#endif
